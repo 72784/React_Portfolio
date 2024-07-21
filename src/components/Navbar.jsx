82c8ad40642a79e38db4090588b585897fdd5015
Parth Shiroya <parthshiroya2001@gmail.com>
2024-07-21 00:24:00 -0700

    logo_1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
       <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 item-center">
-            <img className="mx-2 w-10 color:white "  src={logo} alt="logo" style={{zoom:2.3}}/>
+            <img className="mx-2 w-10 color:white "  src={logo} alt="logo" style={{zoom:3}}/>
         </div>
         <div className="m-8 flex item-center justify-center gap-4 text-2xl">
             <FaLinkedin/>
